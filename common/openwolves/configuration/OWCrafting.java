c61c135e754b7b2eb0ef4df97cd0d800ee750215
Adubbz <a.dubbz@hotmail.com>
2013-09-20 21:14:00 +1000

    Added recipes for turning piles back into blocks

--- common/openwolves/configuration/OWCrafting.java
+++ common/openwolves/configuration/OWCrafting.java
@@ -3,6 +3,7 @@ package openwolves.configuration;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import openwolves.api.piles.OWPileUtils;
+import openwolves.utils.CraftingUtils;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class OWCrafting
@@ -25,8 +26,9 @@ public class OWCrafting
 		GameRegistry.addShapelessRecipe(OWPileUtils.createPileStack(Block.sand.blockID, 4), new Object[] {new ItemStack(Block.sand, 1)});
 		GameRegistry.addShapelessRecipe(OWPileUtils.createPileStack(Block.slowSand.blockID, 4), new Object[] {new ItemStack(Block.slowSand, 1)});
 		
-		//GameRegistry.addShapelessRecipe(new ItemStack(Block.sand, 1), new Object[] {OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1)});
-		//GameRegistry.addShapelessRecipe(new ItemStack(Block.slowSand, 1), new Object[] {OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1)});
+		CraftingUtils.addShapelessPileRecipe(new ItemStack(Block.dirt), new Object[] {OWPileUtils.createPileStack(Block.dirt.blockID, 1), OWPileUtils.createPileStack(Block.dirt.blockID, 1), OWPileUtils.createPileStack(Block.dirt.blockID, 1), OWPileUtils.createPileStack(Block.dirt.blockID, 1)});
+		CraftingUtils.addShapelessPileRecipe(new ItemStack(Block.sand), new Object[] {OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1), OWPileUtils.createPileStack(Block.sand.blockID, 1)});
+		CraftingUtils.addShapelessPileRecipe(new ItemStack(Block.slowSand), new Object[] {OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1), OWPileUtils.createPileStack(Block.slowSand.blockID, 1)});
 	}
 
 	private static void addSmeltingRecipes()
---
+++ common/openwolves/recipes/ShapelessRecipesPile.java
@@ -0,0 +1,88 @@
+package openwolves.recipes;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.world.World;
+import openwolves.api.piles.OWPileUtils;
+
+public class ShapelessRecipesPile implements IRecipe
+{
+    /** Is the ItemStack that you get when craft the recipe. */
+    private final ItemStack recipeOutput;
+
+    /** Is a List of ItemStack that composes the recipe. */
+    public final List recipeItems;
+
+    public ShapelessRecipesPile(ItemStack par1ItemStack, List par2List)
+    {
+        this.recipeOutput = par1ItemStack;
+        this.recipeItems = par2List;
+    }
+
+    @Override
+	public ItemStack getRecipeOutput()
+    {
+        return this.recipeOutput;
+    }
+
+    @Override
+    public boolean matches(InventoryCrafting par1InventoryCrafting, World par2World)
+    {
+    	ArrayList arraylist = new ArrayList(this.recipeItems);
+
+    	for (int i = 0; i < 3; ++i)
+    	{
+    		for (int j = 0; j < 3; ++j)
+    		{
+    			ItemStack itemstack = par1InventoryCrafting.getStackInRowAndColumn(j, i);
+
+    			if (itemstack != null)
+    			{
+    				boolean flag = false;
+    				Iterator iterator = arraylist.iterator();
+
+    				while (iterator.hasNext())
+    				{
+    					ItemStack itemstack1 = (ItemStack)iterator.next();
+    					
+    					ItemStack pileStack = OWPileUtils.getPileBlockFromPileStack(itemstack);
+    					ItemStack pileStack1 = OWPileUtils.getPileBlockFromPileStack(itemstack1);
+
+    					if ((pileStack.itemID == pileStack1.itemID && pileStack.getItemDamage() == pileStack1.getItemDamage()) && itemstack.itemID == itemstack1.itemID && (itemstack1.getItemDamage() == 32767 || itemstack.getItemDamage() == itemstack1.getItemDamage()))
+    					{
+    						flag = true;
+    						arraylist.remove(itemstack1);
+    						break;
+    					}
+    				}
+
+    				if (!flag)
+    				{
+    					return false;
+    				}
+    			}
+    		}
+
+    	}
+
+    	return arraylist.isEmpty();
+    }
+
+    @Override
+	public ItemStack getCraftingResult(InventoryCrafting par1InventoryCrafting)
+    {
+        return this.recipeOutput.copy();
+    }
+
+    @Override
+	public int getRecipeSize()
+    {
+        return this.recipeItems.size();
+    }
+}
+
